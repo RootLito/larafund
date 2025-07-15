488856a44c4663a6692aede5a919aa7745036f80
RootLito <angelitosentillas@gmail.com>
2025-07-15 16:50:35 +0800

    migration fix

--- database/migrations/2025_07_10_093906_add_procurement_fields_to_logs_table.php
+++ database/migrations/2025_07_10_093906_add_procurement_fields_to_logs_table.php
@@ -9,11 +9,9 @@ return new class extends Migration
    public function up(): void
     {
         Schema::table('logs', function (Blueprint $table) {
-            // Add new fields
-            $table->string('procurement_project')->nullable()->after('user_name');
+            // $table->string('procurement_project')->nullable()->after('user_name');
             $table->text('lot_description')->nullable()->after('procurement_project');
 
-            // Drop model_name
             $table->dropColumn('model_name');
         });
     }
@@ -21,9 +19,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('logs', function (Blueprint $table) {
-            // Reverse the changes
             $table->dropColumn(['procurement_project', 'lot_description']);
-            $table->string('model_name')->nullable(); // restore if needed
+            $table->string('model_name')->nullable(); 
         });
     }
 };
